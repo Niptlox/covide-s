a37a373e4bf33f0805d4806e631ae651e1e587d2
Niptlox <GeorgeGoldobin@yandex.ru>
2021-10-14 20:44:54 +0500

    Russian Statistic

--- CovidManager/main/views.py
+++ CovidManager/main/views.py
@@ -10,16 +10,26 @@ import urllib.request, json
 
 "https://api.apify.com/v2/datasets/5JO5GL1h8Qv1CnG0m/items?format=json&limit=10&offset=0&desc=true"
 
-
+month_list = ['янв', 'фев', 'мар', 'апр', 'мая', 'июн',
+              'июл', 'авг', 'сен', 'окт', 'ноя', 'дек']
 def index(request: HttpRequest):
-    with requests.get('https://disease.sh/v3/covid-19/historical/ru?lastdays=30') as url:
+    with requests.get('https://disease.sh/v3/covid-19/historical/ru?lastdays=all') as url:
         data = url.json()
-        cases = list(data["timeline"]["cases"].values())
-        deaths = list(data["timeline"]["deaths"].values())
-        recovered = list(data["timeline"]["recovered"].values())
-        dates = list(data["timeline"]["cases"].keys())
+        tl = data["timeline"]
+        nn = 2
+        cases = list(tl["cases"].values())
+        cases = [cases[i] for i in range(len(cases)) if i % nn == 0]
+        deaths = list(tl["deaths"].values())
+        deaths = [deaths[i] for i in range(len(deaths)) if i % nn == 0]
+        recovered = list(tl["recovered"].values())
+        recovered = [recovered[i] for i in range(len(recovered)) if i % nn == 0]
+        dates = list(tl["cases"].keys())
+        dataf = lambda d: d[1]+" "+month_list[int(d[0]) - 1] + " " + d[2]+"г."
+        dates = [dataf(dates[i].split("/")) for i in range(len(dates)) if i % nn == 0]
+
         print(data)
         print(dates)
         print(cases)
 
-        return render(request, 'main/InfoCountries.html', context={"cases": cases, "deaths": deaths, "recovered": recovered, "dates": dates})
+        return render(request, 'main/InfoRussian.html', context={"cases": cases, "deaths": deaths,
+                                                                   "recovered": recovered, "dates": dates})
--- CovidManager/static/main/js/script.js
+++ CovidManager/static/main/js/script.js
@@ -1,1 +1,28 @@
 // Any of the following formats may be used
+String.format = function() {
+    var s = arguments[0];
+    for (var i = 0; i < arguments.length - 1; i++) {
+    var reg = new RegExp("\\{" + i + "\\}", "gm");
+    s = s.replace(reg, arguments[i + 1]);
+    }
+    return s;
+}
+
+
+function convertDateToStr(date) {
+    dt = new Date(date);
+    dt_now = new Date();
+    diff = dt.getDate() - dt_now.getDate();
+    if (diff == 0) {
+        str_date = "сегодня";
+    } else if (diff == 1) {
+        str_date = "вчера";
+    } else {
+        str_date = dt.toLocaleString('local', {
+                                            year: 'numeric',
+                                            month: 'long',
+                                            day: 'numeric'});
+    }
+    str_date += " в " + dt.toLocaleTimeString('local', {hour:"2-digit", minute:"2-digit"});
+    return str_date
+}
